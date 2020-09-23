78d189477164381f306b2f5bc56d031e3490a569
windfly17 <2838778253@qq,com>
2020-09-23 09:26:38 +0800

    1

---
+++ test_1/cmake-build-debug/CMakeFiles/test_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_1.dir/exp_2.cpp.obj"
+  "test_1.exe"
+  "test_1.exe.manifest"
+  "test_1.lib"
+  "test_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
