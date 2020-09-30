2e219df700cbe345ddb1f78beed48ba4ecbc0bbf
28387 <2838778253@qq.com>
2020-09-30 09:13:01 +0800

    111

---
+++ prime_number/cmake-build-debug/CMakeFiles/prime_number.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prime_number.dir/main.cpp.obj"
+  "prime_number.exe"
+  "prime_number.exe.manifest"
+  "prime_number.lib"
+  "prime_number.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prime_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
