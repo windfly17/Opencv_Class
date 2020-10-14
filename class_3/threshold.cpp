773e6f338354b6b1bf6c02a4b40c852eea5fd9c9
28387 <2838778253@qq.com>
2020-10-14 09:42:23 +0800

    2020_10_14_08:48

--- class_3/threshold.cpp
+++ class_3/threshold.cpp
@@ -14,12 +14,25 @@ void blocksize(int block_size1, void* ){
         block_size = block_size1+1;
 }
 
+Mat convertTo3Channels(const Mat& binImg)
+{
+    Mat three_channel = Mat::zeros(binImg.rows,binImg.cols,CV_8UC3);
+    vector<Mat> channels;
+    for (int i=0;i<3;i++)
+    {
+        channels.push_back(binImg);
+    }
+    merge(channels,three_channel);
+    return three_channel;
+}
+
+
 int main() {
     VideoCapture cap(0, CAP_DSHOW);
 
     int block_size_min = 15;
     int block_size_max = 80;
-    double scale=1;
+    double scale=0.5;
     while(true) {
         cv::Mat frame, thresMat, thresMat_adapt, rFrame_G;
 
@@ -28,19 +41,23 @@ int main() {
         Size ResImgSiz = Size(frame.cols * scale, frame.rows * scale);
         cv::Mat rFrame = Mat(ResImgSiz, frame.type());
         cv::resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
-        cv::Mat showMat = Mat::zeros(Size(rFrame.cols*2+2, rFrame.rows), CV_8UC1);
+        cv::Mat showMat = Mat::zeros(Size(rFrame.cols*3+4, rFrame.rows), CV_8UC3);
 
 
         cv::cvtColor(rFrame, rFrame_G, COLOR_BGR2GRAY);
         cv::threshold(rFrame_G,thresMat,150,255, THRESH_BINARY);
         cv::adaptiveThreshold(rFrame_G, thresMat_adapt, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, block_size, 10);
 
-        thresMat.copyTo(showMat(Rect(0,0, rFrame.cols, rFrame.rows)));
-        thresMat_adapt.copyTo(showMat(Rect(rFrame.cols+2,0, rFrame.cols, rFrame.rows)));
-        cv::rectangle(showMat, Point(rFrame.cols,0), Point(rFrame.cols+2,rFrame.rows), 255, -1);
+        thresMat  = convertTo3Channels(thresMat);
+        thresMat_adapt = convertTo3Channels(thresMat_adapt);
+        rFrame.copyTo(showMat(Rect(0,0, rFrame.cols, rFrame.rows)));
+        thresMat.copyTo(showMat(Rect(rFrame.cols+2,0, rFrame.cols, rFrame.rows)));
+        thresMat_adapt.copyTo(showMat(Rect(rFrame.cols*2+2,0, rFrame.cols, rFrame.rows)));
+        cv::rectangle(showMat, Point(rFrame.cols,0), Point(rFrame.cols+2,rFrame.rows), (0,255,255), -1);
+        cv::rectangle(showMat, Point(rFrame.cols*2,0), Point(rFrame.cols*2+2,rFrame.rows), (0,255,255), -1);
 
 
-        cv::imshow("frame", rFrame);
+//        cv::imshow("frame", rFrame);
         cv::imshow("result", showMat);
         cv::createTrackbar("block_size","result",&block_size_min,block_size_max,blocksize);
 
