33f4fed667e6b1f204c9bccdedecfd31f4c6d31e
28387 <2838778253@qq.com>
2020-11-11 10:22:02 +0800

    2020_11_04_10:29

---
+++ class_6/cmake-build-debug/CMakeFiles/class_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class_6.dir/spin.cpp.obj"
+  "class_6.exe"
+  "class_6.exe.manifest"
+  "class_6.lib"
+  "class_6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
