c5eb4d3e5db93eb2f568ce9b95a4188b2d0b96ea
28387 <2838778253@qq.com>
2020-11-11 10:17:55 +0800

    2020_11_11_10:11

---
+++ class_7/cmake-build-debug/CMakeFiles/class_7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class_7.dir/houghLines_1.cpp.obj"
+  "class_7.exe"
+  "class_7.exe.manifest"
+  "class_7.lib"
+  "class_7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
