a674d0768b14d8b9b29585bd559d7961b2d8195a
28387 <2838778253@qq.com>
2021-01-01 19:28:03 +0800

    2020

---
+++ class_14/cmake-build-debug/CMakeFiles/class_14.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class_14.dir/test.cpp.obj"
+  "class_14.exe"
+  "class_14.exe.manifest"
+  "class_14.lib"
+  "class_14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class_14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
